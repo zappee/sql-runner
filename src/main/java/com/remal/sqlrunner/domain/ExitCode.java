fe315e44275311657c8b1bdf5dc8117ce5c9f024
Arnold Somogyi <arnold.somogyi@ext.ec.europa.eu>
2020-07-15 16:14:22 +0200

    exit code improvement

--- src/main/java/com/remal/sqlrunner/domain/ExitCode.java
+++ src/main/java/com/remal/sqlrunner/domain/ExitCode.java
@@ -10,15 +10,30 @@ package com.remal.sqlrunner.domain;
  * @author arnold.somogyi@gmail.com
  */
 public enum ExitCode {
+
     /**
      * Used in case of the successful program execution.
      */
-    OK(1),
+    OK(ExitCode.OK_EXIT_CODE),
+
+    /**
+     * Command Line Interface error.
+     * This exit code is used in case of missing command line parameter(s).
+     */
+    CLI_ERROR(ExitCode.CLI_ERROR_EXIT_CODE),
 
     /**
      * Used if an unexpected error appeared while executing the SQL statement.
      */
-    SQL_ERROR(2);
+    SQL_ERROR(ExitCode.SQL_ERROR_EXIT_CODE);
+
+    /**
+     * Exit codes constants.
+     */
+    public static final int OK_EXIT_CODE = 0;
+    public static final int CLI_ERROR_EXIT_CODE = 1;
+    public static final int SQL_ERROR_EXIT_CODE = 2;
+
 
     private int exitCodeRepresentation;
 
